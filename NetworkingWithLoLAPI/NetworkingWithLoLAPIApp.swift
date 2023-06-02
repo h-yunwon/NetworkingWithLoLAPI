a9439b6b64d2a93583906241a7b331f44ad900a5
h-yunwon <h_ywon@naver.com>
2023-06-02 11:46:22 +0900

    git init

---
+++ NetworkingWithLoLAPI/NetworkingWithLoLAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingWithLoLAPIApp.swift
+//  NetworkingWithLoLAPI
+//
+//  Created by Yunwon Han on 2023/06/02.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingWithLoLAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
