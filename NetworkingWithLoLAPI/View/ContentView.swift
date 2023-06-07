8d20610a964f01acd909227fef50a6bbf2f11605
h-yunwon <h_ywon@naver.com>
2023-06-07 16:49:13 +0900

    ContentView UI 업데이트

--- NetworkingWithLoLAPI/View/ContentView.swift
+++ NetworkingWithLoLAPI/View/ContentView.swift
@@ -11,7 +11,7 @@ struct ContentView: View {
     @StateObject private var summonerVM = SummonerViewModel()
     @State private var isFormValid: Bool = false
     @State private var isFavorite: Bool = false
-    
+
     func addToProfileInfo() -> ProfileInfo {
         let profileInfo = ProfileInfo(
             summonerName: summonerVM.summonerName,
@@ -27,6 +27,37 @@ struct ContentView: View {
         )
 
         return profileInfo
+    }
+    
+    func setFavorites() -> Favorite {
+        var favorites: Favorite {
+            get {
+                let summonerName = summonerVM.summonerName
+                let summonerLevel = summonerVM.summonerLevel
+                let soloTier = summonerVM.summonerSoloTier
+                let soloRank = summonerVM.summonerSoloRank
+                let soloLeaguePoints = summonerVM.summonerSoloLeaguePoints
+                let flexTier = summonerVM.summonerFlexTier
+                let flexRank = summonerVM.summonerFlexRank
+                let flexLeaguePoints = summonerVM.summonerFlexLeaguePoints
+            //    let summonerProfileIconImageData: Data?
+                let isFavorite = $isFavorite
+                
+                return Favorite(
+                    summonerName: summonerName,
+                    summonerLevel: summonerLevel,
+                    soloTier: soloTier,
+                    soloRank: soloRank,
+                    soloLeaguePoints: soloLeaguePoints,
+                    flexTier: flexTier,
+                    flexRank: flexRank,
+                    flexLeaguePoints: flexLeaguePoints,
+                    isFavorite: false
+                )
+            }
+        }
+        
+        return favorites
     }
     
     func searchSummoner() {
@@ -66,7 +97,53 @@ struct ContentView: View {
             )
             .padding()
             
-            SummonerProfileView(summonerVM: summonerVM, profileInfo: addToProfileInfo(), isFavorite: $isFavorite)
+            VStack {
+                SummonerProfileView(summonerVM: summonerVM, profileInfo: addToProfileInfo(), isFavorite: $isFavorite)
+                    .background(
+                        Color(.systemGray)
+                            .opacity(0.2)
+                    )
+                    .cornerRadius(12)
+                
+                Button(action: {
+                    isFavorite.toggle()
+                    summonerVM.saveFavorites(favorites: setFavorites())
+                }) {
+                    Text("즐겨찾기")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .opacity(0.7)
+                }
+                .frame(maxWidth: .infinity)
+                .background(
+                    Color(.systemGray)
+                        .opacity(0.1)
+                )
+                .cornerRadius(5)
+                
+                SummonerProfileView(summonerVM: summonerVM, profileInfo: addToProfileInfo(), isFavorite: $isFavorite)
+                    .background(
+                        Color(.systemYellow)
+                            .opacity(0.2)
+                    )
+                    .cornerRadius(12)
+                    .padding(.top)
+                Button(action: {
+                    // data 불러오기 
+                }) {
+                    Text("불러오기")
+                        .font(.headline)
+                        .foregroundColor(.black)
+                        .opacity(0.7)
+                }
+                .frame(maxWidth: .infinity)
+                .background(
+                    Color(.systemYellow)
+                        .opacity(0.1)
+                )
+                .cornerRadius(5)
+
+            }
         }
         .padding()
     }
--- NetworkingWithLoLAPI/View/SummonerProfileView.swift
+++ NetworkingWithLoLAPI/View/SummonerProfileView.swift
@@ -11,120 +11,58 @@ struct SummonerProfileView: View {
     var summonerVM: SummonerViewModel
     var profileInfo: ProfileInfo
     
-    @State private var favoriteTier = "unranked"
-    @State private var favoriteRank = ""
-    @State private var favoriteLeaguePoints = 0
-    
+//    @State private var favoriteTier = "unranked"
+//    @State private var favoriteRank = ""
+//    @State private var favoriteLeaguePoints = 0
+//
     @Binding var isFavorite: Bool
 
-    func setFavorites() -> Favorite {
-        var favorites: Favorite {
-            get {
-                let summonerName = profileInfo.summonerName
-                let summonerLevel = profileInfo.summonerLevel
-                let soloTier = profileInfo.soloTier
-                let soloRank = profileInfo.soloRank
-                let soloLeaguePoints = profileInfo.soloLeaguePoints
-                let flexTier = profileInfo.flexTier
-                let flexRank = profileInfo.flexRank
-                let flexLeaguePoints = profileInfo.flexLeaguePoints
-            //    let summonerProfileIconImageData: Data?
-                let isFavorite = $isFavorite
-                
-                return Favorite(
-                    summonerName: summonerName,
-                    summonerLevel: summonerLevel,
-                    soloTier: soloTier,
-                    soloRank: soloRank,
-                    soloLeaguePoints: soloLeaguePoints,
-                    flexTier: flexTier,
-                    flexRank: flexRank,
-                    flexLeaguePoints: flexLeaguePoints,
-                    isFavorite: false
+    var body: some View {
+        HStack(spacing: 10) {
+            profileInfo.summonerProfileIconImage // 소환사 아이콘
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .clipShape(Circle())
+                .overlay(
+                    Text(String(profileInfo.summonerLevel)) // 소환사 레벨
+                        .font(.subheadline)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 7)
+                        .background(Color(.gray))
+                        .cornerRadius(7)
+                    , alignment: .bottom
                 )
+            
+            VStack(alignment: .leading, spacing: 5) {
+                
+                Text(profileInfo.summonerName) // 소환사 이름
+                        .font(.title2)
+                        .fontWeight(.bold)
+
+                
+                HStack {
+                    // 솔로랭크
+                    SummonerRankView(
+                        tier:profileInfo.soloTier,
+                        rank:profileInfo.soloRank,
+                        leaguePoints: profileInfo.soloLeaguePoints
+                    )
+                    
+                    Divider()
+                    
+                    // 자유랭크
+                    SummonerRankView(
+                        tier: profileInfo.flexTier,
+                        rank: profileInfo.flexRank,
+                        leaguePoints: profileInfo.flexLeaguePoints
+                    )
+                }
+                
             }
+            .frame(maxWidth: .infinity, maxHeight: 100)
         }
         
-        return favorites
-    }
-
-
-    var body: some View {
-        VStack {
-            HStack(spacing: 10) {
-                profileInfo.summonerProfileIconImage // 소환사 아이콘
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 80, height: 80, alignment: .center)
-                    .clipShape(Circle())
-                    .overlay(
-                        Text(String(profileInfo.summonerLevel)) // 소환사 레벨
-                            .font(.subheadline)
-                            .foregroundColor(.white)
-                            .padding(.horizontal, 7)
-                            .background(Color(.gray))
-                            .cornerRadius(7)
-                        , alignment: .bottom
-                    )
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    
-                    HStack {
-                        Text(profileInfo.summonerName) // 소환사 이름
-                            .font(.title2)
-                            .fontWeight(.bold)
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            summonerVM.saveFavorites(favorites: setFavorites())
-                        }) {
-                            Text("save")
-    //                        Image(profileInfo.isFavorite ? "star.fill" : "star")
-    //                            .font(.title2)
-    //                            .tint(.yellow)
-                        }
-                    }
-                    
-                    HStack {
-                        // 솔로랭크
-                        SummonerRankView(
-                            tier:profileInfo.soloTier,
-                            rank:profileInfo.soloRank,
-                            leaguePoints: profileInfo.soloLeaguePoints
-                        )
-                        
-                        Divider()
-                        
-                        // 자유랭크
-                        SummonerRankView(
-                            tier: profileInfo.flexTier,
-                            rank: profileInfo.flexRank,
-                            leaguePoints: profileInfo.flexLeaguePoints
-                        )
-                    }
-                    
-                }
-                .frame(maxWidth: .infinity, maxHeight: 100)
-            }
-            
-            Button(action: {
-                if let myFavorites = summonerVM.loadProfileInfo() {
-                    favoriteTier = myFavorites.soloTier
-                    favoriteRank = myFavorites.soloRank
-                    favoriteLeaguePoints = myFavorites.soloLeaguePoints
-                }
-            }, label: {
-                Text("불러오기")
-            })
-            .padding(.top, 20)
-            
-            SummonerRankView(
-                tier:favoriteTier,
-                rank:favoriteRank,
-                leaguePoints: favoriteLeaguePoints
-            )
-        }
         .padding()
     }
 }
